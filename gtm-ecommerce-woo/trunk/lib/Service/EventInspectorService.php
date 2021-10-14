5bb49d641a8133a718d397a35931a5bf174d8a37
Michał Rączka <michal@raczka.me>
2021-10-14 16:43:38 +0200

    Enable demo on different plugin version

--- gtm-ecommerce-woo/trunk/lib/Service/EventInspectorService.php
+++ gtm-ecommerce-woo/trunk/lib/Service/EventInspectorService.php
@@ -8,13 +8,14 @@ namespace GtmEcommerceWoo\Lib\Service;
  */
 class EventInspectorService {
 	protected $wpSettingsUtil;
+	protected $uuidPrefix;
 
 	public function __construct($wpSettingsUtil) {
 		$this->wpSettingsUtil = $wpSettingsUtil;
+		$this->uuidPrefix = substr($this->wpSettingsUtil->getOption('uuid'), 0, -41);
 	}
 
 	public function initialize() {
-		// var_dump($this->wpSettingsUtil->getOption("event_inspector_enabled"));exit;
 		if ($this->wpSettingsUtil->getOption("event_inspector_enabled") === false
 			|| $this->wpSettingsUtil->getOption("event_inspector_enabled") === 'no') {
 			return;
@@ -60,7 +61,7 @@ class EventInspectorService {
 <div id="gtm-ecommerce-woo-event-inspector" style="position: fixed; bottom: 0; right: 0; left: 0; background-color: white;padding: 10px;text-align: center;border-top: 1px solid gray; max-height: 30%; overflow-y: scroll;">
 	<div>Start shopping (add to cart, purchase) to see eCommerce events below, click event to see details.<br />Those events can be forwarded to number of tools in GTM. See <a href="https://tagconcierge.com/google-tag-manager-for-woocommerce/#documentation" target="_blank">documentation</a> for details.</div>
 <?php if ($this->wpSettingsUtil->getOption("event_inspector_demo_mode") === '1'): ?>
-	<div>To learn more about tracking performance <a href="https://app.tagconcierge.com/?demo=woo-free" target="_blank">see DEMO of Tag Concierge App</a> that is a separate product that can integrate with this plugin.</div>
+	<div>To learn more about tracking performance <a href="https://app.tagconcierge.com/?demo=<?php echo $this->uuidPrefix ?>" target="_blank">see DEMO of Tag Concierge App</a> that is a separate product that can integrate with this plugin.</div>
 <?php endif ?>
 	<div id="gtm-ecommerce-woo-event-inspector-list-template" style="display: none;">
 		<li style="cursor: pointer;list-style: none;color: black;font-weight: bold;padding-top: 10px;">{{event}}</li>
