6e5ad4b6e760591a7fded3554502e0565bfbc84e
Michał Frankiewicz <michal@frankiewicz.me>
2023-07-07 19:33:44 +0200

    adds version in release zip

--- dev/scripts/build.sh
+++ dev/scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+RELEASE_VERSION=$(grep -Po 'Version: *\K\S+' gtm-ecommerce-woo.php)
+
 docker-compose run -T --rm php-cli <<INPUT
 
 composer install
@@ -9,10 +11,13 @@ rm vendor -Rf
 
 composer install --no-dev --optimize-autoloader
 
-rm -rf dist/gtm-ecommerce-woo
+rm -rf dist/*
 mkdir -p dist/gtm-ecommerce-woo
 
 cp -R assets src vendor gtm-ecommerce-woo.php readme.txt dist/gtm-ecommerce-woo/
+
 cd dist && zip -r gtm-ecommerce-woo.zip ./gtm-ecommerce-woo
+
+cp gtm-ecommerce-woo.zip "gtm-ecommerce-woo-$RELEASE_VERSION.zip"
 
 INPUT
